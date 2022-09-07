5a7ccf910f4ed776ccea3c65640aa5ed7ed25ed8
nuno80 <104353267+nuno80@users.noreply.github.com>
2022-09-07 14:05:26 +0200

    1 binaries uploaded

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
@@ -8,10 +9,12 @@ export default function Home() {
         <title>Create Next App</title>
       </Head>
 
+      <div>
+        <Navbar />
+      </div>
+
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Benvenuto nel mio sito!</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
